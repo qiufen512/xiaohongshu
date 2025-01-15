7efca44bd59019651601186244331136af1350ed
zebin Cheng <645210207@qq.com>
2025-01-15 21:56:48 +0800

    认证服务整合了MyBatis持久层框架

--- xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/XiaohongshuAuthApplication.java
+++ xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/XiaohongshuAuthApplication.java
@@ -1,9 +1,11 @@
 package com.qiufen.xiaohongshu.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qiufen.xiaohongshu.auth.domain.mapper")
 public class XiaohongshuAuthApplication {
 
     public static void main(String[] args) {
--- xiaohongshu-auth/src/test/java/com/qiufen/xiaohongshu/auth/XiaohongshuAuthApplicationTests.java
+++ xiaohongshu-auth/src/test/java/com/qiufen/xiaohongshu/auth/XiaohongshuAuthApplicationTests.java
@@ -1,13 +1,42 @@
 package com.qiufen.xiaohongshu.auth;
 
+import com.qiufen.framework.common.util.JsonUtils;
+import com.qiufen.xiaohongshu.auth.domain.dataobject.UserDO;
+import com.qiufen.xiaohongshu.auth.domain.mapper.UserDOMapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
+@Slf4j
 class XiaohongshuAuthApplicationTests {
 
+    @Resource
+    private UserDOMapper userDOMapper;
+    /**
+     * 测试插入数据
+     */
     @Test
-    void contextLoads() {
+    void testInsert() {
+        UserDO userDO = UserDO.builder()
+                .username("秋分")
+                .createTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+        userDOMapper.insert(userDO);
+    }
+
+    /**
+     * 查询数据
+     */
+    @Test
+    void testSelect() {
+        // 查询主键 ID 为 4 的记录
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1L);
+        log.info("User: {}", JsonUtils.toJsonString(userDO));
     }
 
 }
