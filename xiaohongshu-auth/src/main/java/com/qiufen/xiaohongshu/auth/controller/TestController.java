0b108e545503de0d2d84dab4b256f92138ec9467
zebin Cheng <645210207@qq.com>
2025-01-15 20:30:27 +0800

    实现了一个日志切面starter组件，后续创建新的服务时，只需添加该starter组件, 然后，为想要打印接口出入参的接口，添加@ApiOperationLog 注解，即可快速使用上日志切面功能了

--- xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/controller/TestController.java
+++ xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/controller/TestController.java
@@ -1,14 +1,27 @@
 package com.qiufen.xiaohongshu.auth.controller;
 
+import com.qiufen.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.qiufen.framework.common.response.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 public class TestController {
 
     @GetMapping("/test")
+    @ApiOperationLog
     public Response<String> test() {
-        return Response.success("Hello, 犬小哈专栏");
+        return Response.success("Hello, qf");
+    }
+
+    @GetMapping("/test2")
+    @ApiOperationLog(description = "测试接口2")
+    public Response<User> test2() {
+        return Response.success(User.builder()
+                .nickName("秋分")
+                .createTime(LocalDateTime.now())
+                .build());
     }
 }
