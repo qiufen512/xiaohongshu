b8250ea4ba2fc5e860be658ed7834ac4b2f892f3
zebin Cheng <645210207@qq.com>
2025-01-15 22:40:43 +0800

    整合了mybatis代码生成器

--- xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/domain/dataobject/UserDO.java
+++ xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/domain/dataobject/UserDO.java
@@ -6,13 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserDO {
-
     private Long id;
 
     private String username;
