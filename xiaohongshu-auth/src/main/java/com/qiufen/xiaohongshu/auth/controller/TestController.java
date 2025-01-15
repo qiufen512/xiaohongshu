a06c4b0b8fe2f7546dc3d4e6fb55f73517e08aad
zebin Cheng <645210207@qq.com>
2025-01-16 00:20:51 +0800

    添加全局异常捕获、接口参数校验

--- xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/controller/TestController.java
+++ xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/controller/TestController.java
@@ -2,6 +2,7 @@ package com.qiufen.xiaohongshu.auth.controller;
 
 import com.qiufen.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.qiufen.framework.common.response.Response;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,7 +30,8 @@ public class TestController {
 
     @PostMapping("/test3")
     @ApiOperationLog(description = "测试接口2")
-    public Response<User> test3(@RequestBody User user) {
+    public Response<User> test3(@RequestBody @Validated User user) {
+        int i = 1 / 0;
         return Response.success(user);
     }
 }
