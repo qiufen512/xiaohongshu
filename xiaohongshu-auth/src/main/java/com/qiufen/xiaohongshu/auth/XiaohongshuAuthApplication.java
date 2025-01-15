0d074cdeb62075e6cc63306da8553659396ef651
zebin Cheng <645210207@qq.com>
2025-01-15 17:05:09 +0800

    引入了 Spring Cloud Alibaba 2022依赖，创建了一个子模块——认证服务

---
+++ xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/XiaohongshuAuthApplication.java
@@ -0,0 +1,13 @@
+package com.qiufen.xiaohongshu.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohongshuAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuAuthApplication.class, args);
+    }
+
+}
---
+++ xiaohongshu-auth/src/test/java/com/qiufen/xiaohongshu/auth/XiaohongshuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiufen.xiaohongshu.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaohongshuAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
