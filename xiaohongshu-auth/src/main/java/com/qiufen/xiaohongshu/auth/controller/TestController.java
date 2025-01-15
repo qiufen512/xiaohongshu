1a6e98ab1fe34f127723e488dff5359ba39f6948
zebin Cheng <645210207@qq.com>
2025-01-15 23:29:44 +0800

    自定义Jackson配置支持LocalDateTime API并封装成starter

--- xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/controller/TestController.java
+++ xiaohongshu-auth/src/main/java/com/qiufen/xiaohongshu/auth/controller/TestController.java
@@ -3,6 +3,8 @@ package com.qiufen.xiaohongshu.auth.controller;
 import com.qiufen.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.qiufen.framework.common.response.Response;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -23,5 +25,11 @@ public class TestController {
                 .nickName("秋分")
                 .createTime(LocalDateTime.now())
                 .build());
+    }
+
+    @PostMapping("/test3")
+    @ApiOperationLog(description = "测试接口2")
+    public Response<User> test3(@RequestBody User user) {
+        return Response.success(user);
     }
 }
